6e85ade13352641e8f23f22ea32f89825502ac3a
Marcelo Janazi <marcelojanazi@gmail.com>
2020-08-25 09:55:26 -0300

    First commit

---
+++ Jnz.MassTransit.OpenTracing/MassTransitOpenTracingExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Jnz.MassTransit.OpenTracing
+{
+    public static class MassTransitOpenTracingExtensions
+    {
+        public static void AddMassTransitOpenTracing(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(MassTransitOpenTracingSendFilter<>));
+            services.AddScoped(typeof(MassTransitOpenTracingConsumeFilter<>));
+        }
+    }
+}
